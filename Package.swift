65fda29fac7b0f3ffcff84e8777afb68b1f07357
Cristiano Fagundes <fagundescristianof@gmail.com>
2026-06-13 17:08:36 -0300

    build: add Sparkle 2.x dependency + Frameworks rpath

--- Package.swift
+++ Package.swift
@@ -5,16 +5,21 @@ let package = Package(
     name: "ProcessMonitor",
     platforms: [.macOS(.v13)],
     dependencies: [
-        .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.36.0")
+        .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.36.0"),
+        .package(url: "https://github.com/sparkle-project/Sparkle", from: "2.6.0")
     ],
     targets: [
         .executableTarget(
             name: "ProcessMonitor",
             dependencies: [
-                .product(name: "Sentry", package: "sentry-cocoa")
+                .product(name: "Sentry", package: "sentry-cocoa"),
+                .product(name: "Sparkle", package: "Sparkle")
             ],
             path: "ProcessMonitor",
-            resources: [.process("Resources")]
+            resources: [.process("Resources")],
+            linkerSettings: [
+                .unsafeFlags(["-Xlinker", "-rpath", "-Xlinker", "@executable_path/../Frameworks"])
+            ]
         ),
         .testTarget(
             name: "ProcessMonitorTests",
